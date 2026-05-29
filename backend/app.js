d5e302e15354883378c9180de7b8e2c418115a8a
Mohammad Ammash <mohammadammash99@gmail.com>
2026-05-29 13:49:07 +0300

    add mongodb connection

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes will go here
+// app.use('/api/contact', require('./routes/contactRoutes'));
+
+module.exports = app;
